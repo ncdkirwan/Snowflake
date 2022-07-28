68b30ddbdb5be71e8277b5e067b5ddc748b7d7d6
ncdkirwan <54441385+ncdkirwan@users.noreply.github.com>
2022-07-28 12:41:20 -0400

    Update streamlit_app.py
    Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it to the screen as the table
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_accout(), current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
